ce7e683d79e0b3a0d2426aa517002cb0e2afeb20
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2022-02-12 13:45:13 -0300

    Alterações no html, css e js

--- script.js
+++ script.js
@@ -1,40 +1,79 @@
-const div = document.querySelector('.teste')
 
+const div = document.querySelector('.grow');
+const opcoes = document.querySelectorAll('.opcoes')
+
+
+
+// Constroi array com links de imagens
 const id = async () => {
     let arrayVazioId = [];
-    for (let index = 1000; index < 1010; index += 20) {
+    for (let index = 1; index < 80; index += 20) {
       const url = `https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=${index}`;
       const chamaApi = await fetch(url);
       const response = await chamaApi.json();
       const arrayDeObjetos = response.data;
       arrayDeObjetos.find((element) => {
-          console.log(element);
-        arrayVazioId.push(element.attributes.posterImage.large);
+        if(!element.attributes.description.includes('sex') &&
+        !element.attributes.canonicalTitle.includes('Ghost in the Shell') &&
+        !element.attributes.description.includes('hentai') &&
+        !element.attributes.description.includes('girls') &&
+        !element.attributes.canonicalTitle.includes('Rozen Maiden') &&
+        !element.attributes.canonicalTitle.includes('DearS')) {
+            console.log(element)
+            arrayVazioId.push(element.attributes.posterImage.large);
+        }
       });
     }
+    /* console.log(arrayVazioId) */
     return arrayVazioId;
   };
-  
+
+// Constroi array de Ids
 const pegaId = async () => {
     let arrayVazioIds = [];
-    for (let index = 1000; index < 1010; index += 20) {
+    for (let index = 1; index < 80; index += 20) {
       const url = `https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=${index}`;
       const chamaApi = await fetch(url);
       const response = await chamaApi.json();
       const arrayDeObjetos = response.data;
       arrayDeObjetos.find((element) => {
-          /* console.log(element); */
-          arrayVazioIds.push(element.id);
+        if(!element.attributes.description.includes('sex') &&
+        !element.attributes.canonicalTitle.includes('Ghost in the Shell') &&
+        !element.attributes.description.includes('hentai') &&
+        !element.attributes.description.includes('girls') &&
+        !element.attributes.canonicalTitle.includes('Rozen Maiden') &&
+        !element.attributes.canonicalTitle.includes('DearS')) {
+            arrayVazioIds.push(element.id);
+        }
         });
     }
       return arrayVazioIds;
 }
+    // Cria elementos e adiciona todos como filho da tag div
+    const manipulacao = (im) => {
+        const principal = document.querySelector('.main');
+        const para = document.querySelector('.lorem');
+        const para2 = document.querySelector('.synopsis')
+        const div = document.querySelector('.grow');
+        const h1 = document.querySelector('.nome-anime');
+        const nota = document.querySelector('.nota');
+        const lorem = document.querySelector('.lorem');
+        const imagem = document.createElement('img');
+        
+        imagem.src = im.attributes.posterImage.large
+        h1.innerText = im.attributes.canonicalTitle;
+        para.innerText = im.attributes.synopsis;
+        
+        div.appendChild(imagem);
+        principal.style.backgroundImage = `url(${im.attributes.posterImage.large})`
+        /* div.appendChild(img); */
+    }
 
-/* pegaId().then((data) => console.log(data)) */
-
+  
+  // 
   const lida = id().then((data) => {
     let contador = 0;
-    data.forEach((element, index) => {
+    data.forEach((element) => {
         const img = document.createElement('img');
         img.src = element;
         img.classList.add('banners-animes');
@@ -50,7 +89,7 @@ const pegaId = async () => {
           element.addEventListener('click', async () => {
               const capturaID = element.id;
               console.log(capturaID)
-              for (let index = 1000; index < 1010; index += 20) {
+              for (let index = 1; index < 80; index += 20) {
                 const url = `https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=${index}`;
                 const chamaApi = await fetch(url);
                 const response = await chamaApi.json();
@@ -58,14 +97,32 @@ const pegaId = async () => {
                 arrayDeObjetos.find((element) => {
                     if (capturaID === element.id) {
                         console.log(element);
+                        manipulacao(element);
                     }
                 });
             }
           });
-      }) 
+      });
     })
   })
+
+  opcoes.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      if(event.target === opcoes[0]) { // Categorias
+        console.log('Você clicou na primeira opção :D');
+      }
+      if (event.target === opcoes[1]) { // Top Ratings
+        console.log('Você clicou na segunda opção :D');
+      }
+      if (event.target === opcoes[2]) { // Lista
+        console.log('Você clicou na terceira opção')
+      }
+      if (event.target === opcoes[3]) { // Descubra
+        console.log('Você clicou na quarta opção')
+      }
+    })
+  });
   
   window.onload = async () => {
-    
+
   };
